25d3aae9f4d30ebac2ead08273894bb8f28aad33
Ross Ryles <ross.ryles@gmail.com>
2021-06-12 11:25:19 +0100

    Matrix multiplication

--- 1.1/1.1.py
+++ 1.1/1.1.py
@@ -2,22 +2,15 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # Suppress warning that it can't use CUDA
 import tensorflow as tf
 
-Matrix_one = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
-Matrix_two = tf.constant([[2,2,2],[2,2,2],[2,2,2]])
+Matrix_one = tf.constant([[2,3],[3,4]])
+Matrix_two = tf.constant([[2,3],[3,4]])
 
 @tf.function
-def add():
-    add_1_operation = tf.add(Matrix_one, Matrix_two)
-    return add_1_operation
+def mathmul():
+  return tf.matmul(Matrix_one, Matrix_two)
 
 
+mul_operation = mathmul()
 
 print ("Defined using tensorflow function :")
-add_1_operation = add()
-print(add_1_operation)
-print ("Defined using normal expressions :")
-add_2_operation = Matrix_one + Matrix_two
-print(add_2_operation)
-
-print("Product :")
-print(Matrix_one * Matrix_two)
+print(mul_operation)
