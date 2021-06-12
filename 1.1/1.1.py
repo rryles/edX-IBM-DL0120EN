06bc98e14c4a56dc79b9a8d09fa9fbbbb45ec698
Ross Ryles <ross.ryles@gmail.com>
2021-06-12 11:23:42 +0100

    Matrix operations

--- 1.1/1.1.py
+++ 1.1/1.1.py
@@ -1,28 +1,23 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # Suppress warning that it can't use CUDA
 import tensorflow as tf
 
-print (tf.__version__)
-
-a = tf.constant([2], name = 'constant_a')
-b = tf.constant([3], name = 'constant_b')
+Matrix_one = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
+Matrix_two = tf.constant([[2,2,2],[2,2,2],[2,2,2]])
 
 @tf.function
-def add(a, b):
-    c = tf.add(a, b)
-    print(c)
-    return c
+def add():
+    add_1_operation = tf.add(Matrix_one, Matrix_two)
+    return add_1_operation
 
-result = add(a, b)
-tf.print(result[0])
 
-Scalar = tf.constant(2)
-Vector = tf.constant([5,6,2])
-Matrix = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
-Tensor = tf.constant( [ [[1,2,3],[2,3,4],[3,4,5]] , [[4,5,6],[5,6,7],[6,7,8]] , [[7,8,9],[8,9,10],[9,10,11]] ] )
 
-print ("Scalar (1 entry):\n %s \n" % Scalar)
+print ("Defined using tensorflow function :")
+add_1_operation = add()
+print(add_1_operation)
+print ("Defined using normal expressions :")
+add_2_operation = Matrix_one + Matrix_two
+print(add_2_operation)
 
-print ("Vector (3 entries) :\n %s \n" % Vector)
-
-print ("Matrix (3x3 entries):\n %s \n" % Matrix)
-
-print ("Tensor (3x3x3 entries) :\n %s \n" % Tensor)
+print("Product :")
+print(Matrix_one * Matrix_two)
