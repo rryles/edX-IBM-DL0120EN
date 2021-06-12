c15428278b7c63fec3d162904e29a1da6f0ff0a6
Ross Ryles <ross.ryles@gmail.com>
2021-06-12 12:10:01 +0100

    Perform linear regression

--- 1.2/linear-regression.py
+++ 1.2/linear-regression.py
@@ -9,16 +9,47 @@ import tensorflow as tf
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize'] = (10, 6)
+df = pd.read_csv("FuelConsumption.csv")
 
-X = np.arange(0.0, 5.0, 0.1)
+train_x = np.asanyarray(df[['ENGINESIZE']])
+train_y = np.asanyarray(df[['CO2EMISSIONS']])
 
-a = 1
-b = 0
+# Model parameters
+a = tf.Variable(30.0)
+b = tf.Variable(1.0)
 
-Y= a * X + b 
+# Model
+def h(x):
+   y = a*x + b
+   return y
 
-plt.plot(X, Y) 
-plt.ylabel('Dependent Variable')
-plt.xlabel('Indepdendent Variable')
+# Loss function that we will minimise
+def loss_object(y, train_y):
+    return tf.reduce_mean(tf.square(y - train_y))
+
+learning_rate = 0.01
+train_data = []
+loss_values = []
+
+training_epochs = 200
+
+# Train Model
+for epoch in range(training_epochs):
+    with tf.GradientTape() as tape:
+        # calculate loss
+        y_predicted = h(train_x)
+        loss_value = loss_object(train_y, y_predicted)
+        loss_values.append(loss_value)
+
+        # get gradients
+        gradients = tape.gradient(loss_value, [a, b])
+
+        # adjust weights
+        a.assign_sub(gradients[0] * learning_rate)
+        b.assign_sub(gradients[1] * learning_rate)
+        if epoch % 5 == 0:
+            train_data.append([a, b])
+
+# plot loss values
+plt.plot(loss_values, 'ro')
 plt.show()
