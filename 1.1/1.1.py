9319243017c4048c19fcbbf6e313bb45a4d40f1a
Ross Ryles <ross.ryles@gmail.com>
2021-06-12 11:35:57 +0100

    Variables - a simple counter

--- 1.1/1.1.py
+++ 1.1/1.1.py
@@ -2,15 +2,13 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # Suppress warning that it can't use CUDA
 import tensorflow as tf
 
-Matrix_one = tf.constant([[2,3],[3,4]])
-Matrix_two = tf.constant([[2,3],[3,4]])
+v = tf.Variable(0)
 
 @tf.function
-def mathmul():
-  return tf.matmul(Matrix_one, Matrix_two)
+def increment(v):
+    v = tf.add(v, 1)
+    return v
 
-
-mul_operation = mathmul()
-
-print ("Defined using tensorflow function :")
-print(mul_operation)
+for _ in range(3):
+    v = increment(v)
+    print(v)
